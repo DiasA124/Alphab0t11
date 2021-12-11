d6c570116470acb2c08f411eb1ba08f4e0b93432
DiasA124 <95956570+DiasA124@users.noreply.github.com>
2021-12-11 18:59:27 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,33 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY DIASBOTZ
 
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ 88810 0815 6388 1736
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 3901 0815 6388 1736
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ 8810 0815 6388 1736
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ 8528 0815 6388 1736
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0815 6388 1736
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0815 6388 1736
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0815 6388 1736
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama D**S
 
 *📲 PULSA*
 
-Tidak tersedia
+   TELKOMSEL 
+        
+            ㅁ 0812 1840 4232
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
